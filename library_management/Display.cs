0486ffdbe3c69013bd9a9367d440eeb985eaa52b
fadi <shami55shami66@gmail.com>
2022-09-26 16:06:23 +0300

    orginaizethecode

--- library_management/Display.cs
+++ library_management/Display.cs
@@ -23,9 +23,14 @@ namespace library_management
                         Console.WriteLine($"id book: {book.Id}," +
                             $" name book: {book.Title_Book}," +
                             $" number copes:{book.Count_Copes} " );
+
+                        Console.WriteLine("---------------------------" +
+                        "-------------------------------------");
                     }
                     Console.WriteLine("total of copes for books=" +
                                     books.Sum(c => c.Count_Copes));
+                    Console.WriteLine("---------------------------" +
+                        "-------------------------------------");
                 } 
             }
         }
@@ -43,9 +48,44 @@ namespace library_management
                         Console.WriteLine($"id book:[{book.Id}]," +
                             $" name book: [{book.Title_Book}]," +
                             $" number copes:[{book.Count_Copes}] ");
-               
+
+                       Console.WriteLine("---------------------------" +
+                            "-------------------------------------");
                 }
             }
+        }
+        /// <summary>
+        /// This function displays the total number of books in the store
+        /// </summary>
+        public static void totalcountbooks()
+        {
+            using (library_Db context = new library_Db())
+            {
+                var book = context.Books?.ToList();
+                Console.WriteLine("The total number of books in stock is: ["
+                                  + (book?.Count()) + "]");
+
+                Console.WriteLine("---------------------------" +
+                          "-------------------------------------");
+            }
+        }
+        /// <summary>
+        /// This function shows the user a list of available options
+        /// </summary>
+        /// <returns></returns>
+        public static string checklistfunction()
+        {
+
+            Console.WriteLine("enter (1) for show information all books \n" +
+                              "enter (2) View information for a specific book\n" +
+                              "enter (3) for delete a specific book\n" +
+                              "enter (4) for apdate title and price a specific book\n"+
+                              "enter (5) To log out of the application");
+            Console.WriteLine("----------------------------------------------------------");
+            string? select = Console.ReadLine();
+
+
+            return select;
         }
     }
 }
